7da52b653e8ae182fbe856c3166f4f4c57204204
Mikle <misha.zakh@gmail.com>
2024-03-29 22:45:52 +0100

    statuses

--- src/code/code.controller.ts
+++ src/code/code.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { CodeService } from './code.service';
+import { Response } from 'express';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('code')
 export class CodeController {
     constructor(private readonly codeService: CodeService) { }
 
-
+    @ApiResponse({ description: "Success", status: 202 })
+    @ApiResponse({ description: "Fail", status: 404 })
     @Post(":activationCode")
-    async validate(@Param('activationCode') activationCode: string): Promise<string> {
+    async validate(@Param('activationCode') activationCode: string, @Res() res: Response) {
         console.log(activationCode)
         const isValid = await this.codeService.validateCode(activationCode);
-        return isValid ? 'Success' : 'Fail';
+
+
+
+        isValid ? res.status(202).send("Success") : res.status(404).send("Fail");
+
     }
 
     @Get()
