6a61215b2280b947b9713ca315a52a23009eee15
Mikle <misha.zakh@gmail.com>
2024-04-01 14:49:00 +0200

    ssl changes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as https from 'https';
+import { promises as fs } from 'fs'; // Using promises for asynchronous approach
+
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFile("/etc/letsencrypt/live/diamon-drush.io/privkey.pem"),
+      cert: fs.readFile("/etc/letsencrypt/live/diamon-drush.io/fullchain.pem")
+    }
+  });
 
   const config = new DocumentBuilder()
     .setTitle('BassHead Plugin Validation API')
@@ -18,7 +26,8 @@ async function bootstrap() {
 
 
 
-  await app.listen(3000);
+  //await app.listen(3000);
+  await app.listen(443);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
